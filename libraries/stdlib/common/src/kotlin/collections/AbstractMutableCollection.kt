867b2a8ea29fad40d388ed7e2cd1a25ccaa4a653
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-11-02 00:06:37 +0300

    Correct abstract mutable collections declarations in kotlin-stdlib-common

--- libraries/stdlib/common/src/kotlin/collections/AbstractMutableCollection.kt
+++ libraries/stdlib/common/src/kotlin/collections/AbstractMutableCollection.kt
@@ -6,11 +6,11 @@
 package kotlin.collections
 
 /**
- * Provides a skeletal implementation of the [MutableSet] interface.
+ * Provides a skeletal implementation of the [MutableCollection] interface.
  *
- * @param E the type of elements contained in the set. The set is invariant on its element type.
+ * @param E the type of elements contained in the collection. The collection is invariant on its element type.
  */
-public expect abstract class AbstractMutableSet<E> : MutableSet<E> {
+public expect abstract class AbstractMutableCollection<E> : MutableCollection<E> {
     protected constructor()
 
     abstract override val size: Int
@@ -28,3 +28,4 @@ public expect abstract class AbstractMutableSet<E> : MutableSet<E> {
     override fun retainAll(elements: Collection<E>): Boolean
     override fun clear(): Unit
 }
+
