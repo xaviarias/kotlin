47ea2066af93c83ba614a69212a30b68188c538c
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2018-11-12 13:27:02 +0100

    ~add enum tests

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/groovyInterop/lib/src/main/groovy/ClassWithReferenceToInner.groovy
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/groovyInterop/lib/src/main/groovy/ClassWithReferenceToInner.groovy
@@ -4,6 +4,18 @@ public class ClassWithReferenceToInner {
     public void f2(Outer.Nested nested) {}
 }
 
+@interface Foo {}
+
 class Outer {
-    class Nested {}
+    class Nested {
+        Nested(@Foo String name) {
+
+        }
+    }
+}
+
+enum GEnum {
+    FOO("123");
+
+    GEnum(@Foo String value) {}
 }
